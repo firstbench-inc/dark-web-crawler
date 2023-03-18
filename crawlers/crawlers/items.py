f6957db373d59a7a84da4d649b860571103e77e1
MBUYT0N <shurith03@gmail.com>
2023-03-18 12:32:03 +0530

    black'd

--- crawlers/crawlers/items.py
+++ crawlers/crawlers/items.py
@@ -6,15 +6,16 @@ from scrapy.item import Item, Field
 
 
 char = (chr(i) for i in range(0x110000))
-control = ''.join(i for i in char if unicodedata.categroy(i) == "Cc")
+control = "".join(i for i in char if unicodedata.categroy(i) == "Cc")
 re_control = re.compile("[%s]" % re.escape(control))
-    
+
+
 class Link(Item):
     source_url = Field()
     target_url = Field()
     target_document = Field()
-    
+
+
 class AuthorityScore(Item):
     SHA_hash_url = Field()
     pagerank_score = Field()
-    
--- crawlers/crawlers/middlewares.py
+++ crawlers/crawlers/middlewares.py
@@ -4,32 +4,40 @@ import random
 import logging
 from urllib.parse import urlparse
 
+
 class Proxies(object):
     """proxy"""
 
     def incoming_requests(self, request, spider):
         uri = urlparse(request.url)
         domain = f"{uri.scheme}://{uri.netloc}/"
-        
+
         if ".onion" in domain:
-            non_http_link = domain.split(".")[-2].replace("https://", "").replace("http://", "")
-            
-            if domain[-7:-1] != ".onion" or len(non_http_link) != 16 or len(non_http_link) !=56:
+            non_http_link = (
+                domain.split(".")[-2].replace("https://", "").replace("http://", "")
+            )
+
+            if (
+                domain[-7:-1] != ".onion"
+                or len(non_http_link) != 16
+                or len(non_http_link) != 56
+            ):
                 logging.info("Not valid .onion link, ignoring")
                 raise IgnoreRequest()
-            
-            #proxy lists
+
+            # proxy lists
             if uri.scheme == "https:":
                 proxy_list = settings.get(HTTPS_TOR_PROXIES)
             else:
                 proxy_list = settings.get(HTTP_TOP_PROXIES)
-            
+
             edited_uri = uri.replace(".onion", "")
             seed_hash = f"{edited_uri.netloc}"
             random.seed(seed_hash)
-            
+
             request.meta["proxy"] = random.choice(proxy_list)
-            
+
+
 class LimitDomains(object):
     def incoming_requests(self, request, spider):
         host_name = urlparse(request.url).hostname.split(".")
@@ -37,5 +45,3 @@ class LimitDomains(object):
             request.meta["proxy"] = ""
             logging.info(f"Ignoring {request.url}, too many domains")
             raise IgnoreRequest()
-                
-        
