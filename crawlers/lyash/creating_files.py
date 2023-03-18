e9bf8322daab9c1452c3a4d264be11d7d4bdcb91
MBUYT0N <shurith03@gmail.com>
2023-03-18 16:05:48 +0530

    viji god

---
+++ crawlers/lyash/creating_files.py
@@ -0,0 +1,16 @@
+t=input("Enter the word\n")
+f=open("search_results.txt")
+x=f.readlines()
+for i in range(0,len(x)):
+    w=x[i]
+    n=w.replace('\n','')
+
+    f1=open(str(n))
+    b=f1.readlines()
+    for y in b:
+        if t in y:
+
+            f2=open("sample.txt",'a')
+            f2.write(str(y))
+
+
