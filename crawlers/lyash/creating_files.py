a8e364f6cab2d9cfd7e91f3fe71416bc65954a0f
MBUYT0N <shurith03@gmail.com>
2023-03-18 20:36:42 +0530

    eh

--- crawlers/lyash/creating_files.py
+++ crawlers/lyash/creating_files.py
@@ -1,16 +1,14 @@
-t=input("Enter the word\n")
-f=open("search_results.txt")
-x=f.readlines()
-for i in range(0,len(x)):
-    w=x[i]
-    n=w.replace('\n','')
+def files():
+    t = input("Enter the word\n")
+    f = open("search_results.txt")
+    x = f.readlines()
+    for i in range(0, len(x)):
+        w = x[i]
+        n = w.replace("\n", "")
 
-    f1=open(str(n))
-    b=f1.readlines()
-    for y in b:
-        if t in y:
-
-            f2=open("sample.txt",'a')
-            f2.write(str(y))
-
-
+        f1 = open(str(n))
+        b = f1.readlines()
+        for y in b:
+            if t in y:
+                f2 = open(f"{str(n)}.txt", "a")
+                f2.write(str(y))
